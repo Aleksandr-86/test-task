9cbced288a81b53b59a990ee50fb028be6e9c9f3
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-04-10 15:19:18 +0300

    feat: add tailwindcss

--- src/pages/TheSignInPage.vue
+++ src/pages/TheSignInPage.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import TheSignIn from '@/components/TheSignIn.vue'
+import TheOAuth from '@/components/TheOAuth.vue'
+
+// defineProps<{
+//   msg: string
+// }>()
+</script>
+
 <template>
-  <div>страница авторизации</div>
+  <div class=".flex">
+    <TheSignIn />
+    <TheOAuth />
+  </div>
 </template>
